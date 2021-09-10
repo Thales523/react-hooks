008230d17bd05ad316a4e8d4a3253fd3c2b756bd
Thales523 <luizgato25@terra.com.br>
2021-09-10 16:07:47 +0000

    Aula 05 -Hooks

--- hooks/src/components/layout/Content.jsx
+++ hooks/src/components/layout/Content.jsx
@@ -1,26 +1,26 @@
 import './Content.css'
 import React from "react"
 import { Switch, Route } from 'react-router-dom'
-import Home from '../../views/contents/Home'
-import Conteudo01 from '../../views/contents/Conteudo01'
-import Conteudo02 from '../../views/contents/Conteudo02'
-import Conteudo03 from '../../views/contents/Conteudo03'
+import Hooks from '../../views/contents/Hooks'
+import UseState from '../../views/contents/UseState'
+import UseEffect from '../../views/contents/UseEffect'
+import UseContext from '../../views/contents/UseContext'
 import NaoEncontrado from '../../views/contents//NaoEncontrado'
 
 const Content = props => (
     <main className="Content">
         <Switch>
             <Route exact path="/">
-                <Home />
+                <Hooks />
             </Route>
-            <Route path="/conteudo01/:usuario">
-                <Conteudo01 />
+            <Route path="/UseState">
+                <UseState />
             </Route>
-            <Route path="/conteudo02">
-                <Conteudo02 />
+            <Route path="/UseEffect">
+                <UseEffect />
             </Route>
-            <Route path="/conteudo03">
-                <Conteudo03 />
+            <Route path="/UseContext">
+                <UseContext />
             </Route>
             <Route path="*">
                 <NaoEncontrado />
